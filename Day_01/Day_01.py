1ae8ac1cc57e80953ff263c94e00a859e61b05ea
İbrahim Altıkat <65372029+ialtikat@users.noreply.github.com>
2022-06-23 21:14:47 +0300

    Update Day_01.py

--- Day_01/Day_01.py
+++ Day_01/Day_01.py
@@ -7,13 +7,15 @@ print(type(a))
 a=4.5
 print(type(a))
 
-#<class 'int'>
-#<class 'str'>
-#<class 'float'>
+Output
+<class 'int'>
+<class 'str'>
+<class 'float'>
 
 a="Python İle Fonksiyonel Programlama"
 print(dir(a))
 
+Output
 ['__add__', '__class__', '__contains__', '__delattr__', '__dir__', '__doc__', '__eq__', 
  '__format__', '__ge__', '__getattribute__', '__getitem__', '__getnewargs__', '__gt__',
  '__hash__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__',
