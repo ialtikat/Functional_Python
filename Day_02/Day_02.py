87165254a3f9129bd2e549dd8a875e553f98a26a
İbrahim Altıkat <65372029+ialtikat@users.noreply.github.com>
2022-06-27 17:39:41 +0300

    Add files via upload

---
+++ Day_02/Day_02.py
@@ -0,0 +1,19 @@
+a = 2
+b = a**2
+print("{}'nın karesi = {}".format(a,b))
+
+# Output
+# 2'nın karesi = 4
+
+a = 2/3
+print("a = ",a)
+print("a ={:.2f}".format(a))
+
+# Output
+# a =  0.6666666666666666
+# a =0.67
+
+print("{1} - {2} - {0}".format(3,1,2))
+
+# Output
+# 1 - 2 - 3
