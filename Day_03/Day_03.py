7a778ed4b758ffd56469a0078f42af04293aaf00
İbrahim Altıkat <65372029+ialtikat@users.noreply.github.com>
2022-06-28 15:15:43 +0300

    Add files via upload

---
+++ Day_03/Day_03.py
@@ -0,0 +1,57 @@
+#tek satırda açıklama için '#' işaretini kullanmalıyız.
+
+"""  
+   Çoklu satır kullanacağımız zaman
+   yapmamamız gereken 3 tırnak içine 
+   açıklama metnini yazmak. 
+"""
+#Operatörler
+"""
+    + 	Toplama
+    - 	Çıkarma
+    *	Çarpma
+    ** 	Üst alma
+    / 	Bölme
+    // 	Tam sayı bölme
+    % 	Bölmeden kalan
+    += 	Topla ve eşitle
+    -= 	Çıkar ve eşitle
+    *= 	Çarp ve eşitle
+    /= 	Böl ve eşitle
+
+"""
+a = 9
+b = a // 4
+c = a % 4
+d = a + b
+e = a - b
+f = a * b
+g = a / b
+
+print(
+      "a =", a
+      ,"b =", b
+      ,"c =", c
+      ,"d =", d
+      ,"e =", e
+      ,"f =", f
+      ,"g =", g
+)
+
+# Output
+# a = 9 b = 2 c = 1 d = 11 e = 7 f = 18 g = 4.5
+
+a += b
+b -= c
+d *= e
+e /= c
+
+print(
+       "a =", a
+      ,"b =", b
+      ,"d =", d
+      ,"e =", e
+)
+
+# Output
+# a = 11 b = 1 d = 77 e = 7.0
