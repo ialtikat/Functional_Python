2e698d1a6848ac82da912323dfc5f7c484acf6aa
İbrahim Altıkat <65372029+ialtikat@users.noreply.github.com>
2022-07-06 10:26:24 +0300

    Add files via upload

---
+++ Day_05/Day_05.py
@@ -0,0 +1,18 @@
+a = 115
+b = 100
+if a > b: print("A Büyüktür B")
+
+#	Output
+#	A Büyüktür B
+
+print("A Büyüktür B") if a > b else print("B Büyüktür A")
+
+#	Output
+#	A Büyüktür B
+
+a = 30
+b = 40
+print("A Büyüktür B") if a > b else print("B Büyüktür A")
+
+#	Output
+#	B Büyüktür A
