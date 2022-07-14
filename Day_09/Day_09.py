829fba8e9fac460f296ae96920877d37f35a9cbc
İbrahim Altıkat <65372029+ialtikat@users.noreply.github.com>
2022-07-14 12:23:11 +0300

    Add files via upload

---
+++ Day_09/Day_09.py
@@ -0,0 +1,14 @@
+"""
+    While döngüsü for döngüsünden farklı olarak döngü değişkeni ve döngü içinde bu değişkenin güncellenmesine ihtiyaç vardır.
+"""
+x = 10
+t = 0
+i = 0
+while i < x:
+    t += i
+    i += 1
+
+print("Toplam : ",t)
+
+# Output
+# Toplam :  45
