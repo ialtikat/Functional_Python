5b874811115c29c2bc9097c75835c6e80555a5a9
İbrahim Altıkat <65372029+ialtikat@users.noreply.github.com>
2022-07-21 21:45:49 +0300

    Add files via upload

---
+++ Day_14/Day_14.py
@@ -0,0 +1,19 @@
+>>> import math
+>>> math.pow(4,3)
+64.0
+>>> min(3,4,5)
+3
+>>> math.ceil(2.01)
+3
+>>> math.floor(2.01)
+2
+>>> math.sqrt(64)
+8.0
+>>> math.pi
+3.141592653589793
+>>> dir(math)
+['__doc__', '__loader__', '__name__', '__package__', '__spec__', 'acos', 'acosh', 'asin', 'asinh', 'atan', 'atan2', 'atanh',
+ 'ceil', 'comb', 'copysign', 'cos', 'cosh', 'degrees', 'dist', 'e', 'erf', 'erfc', 'exp', 'expm1', 'fabs', 'factorial', 'floor',
+ 'fmod', 'frexp', 'fsum', 'gamma', 'gcd', 'hypot', 'inf', 'isclose', 'isfinite', 'isinf', 'isnan', 'isqrt', 'lcm', 'ldexp', 'lgamma',
+ 'log', 'log10', 'log1p', 'log2', 'modf', 'nan', 'nextafter', 'perm', 'pi', 'pow', 'prod', 'radians', 'remainder', 'sin', 'sinh',
+ 'sqrt', 'tan', 'tanh', 'tau', 'trunc', 'ulp']
